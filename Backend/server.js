541f7c8352ed16ab19414dd17b5c9ecf0f4e3c37
tekchand87 <tekchandyadav45@gmail.com>
2026-05-27 23:35:26 +0530

    make a usermodel

---
+++ Backend/server.js
@@ -0,0 +1,7 @@
+import app from './src/app.js'
+import connectDB from './src/config/database.js'
+
+connectDB()
+app.listen(3000,()=>{
+  console.log("server is running of 3000 port")
+})
---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
+
+const userSchema = new mongoose.Schema({
+  name : {
+    type : String,
+    required : [true,"username is required"]
+  },
+  email : {
+    type : String,
+    required : [true,"Email is required"],
+    unique : [true, "Email already exist"],
+    trim : true,
+    lowercase : true,
+    match : [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/]
+  },
+  password : {
+    type : String,
+    required : [true,"password is required"],
+    minlength :[6,"password length must be minmum length 6"],
+    select : false
+  }
+},{
+  timestamps : true
+})
+
+
+userSchmea.pre("save",async function(){
+  if(!this.isModified("password")){
+    return ;
+  }
+
+  const hash = await bcrypt.hash(this.password,10)
+
+  this.password = hash;
+  return 
+})
+
+userSchema.methods.comparePassword= async function(password){
+  console.log(password,this.password);
+
+  return await bcrypt.compare(this.password,password);
+}
+
+const userModel = mongoose.model('user',userSchema)
+
+export default userModel
